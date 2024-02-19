a87912904795f4a5de4dae4f6bc9bbec70fe0bbc
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-02-19 13:50:17 +0530

    Refactor SelectedMusic component

--- app/music/[music]/[eventinfo]/page.tsx
+++ app/music/[music]/[eventinfo]/page.tsx
@@ -1,27 +1,15 @@
 import ArtistName from "@/components/Cards/ArtistName";
-import { getAuthById, getMusic, getMusicbySlug } from "@/sanity/sanity-util";
+import { getMusicbySlug } from "@/sanity/sanity-util";
 import Image from "next/image";
 import React from "react";
 
 const SelectedMusic = async ({ params }: { params: { eventinfo: string } }) => {
   const music = await getMusicbySlug(params.eventinfo);
-  //   const artist = music.map((p) => p.artist);
-  //   const artistbyid = await getAuthById(id);
-  /*
-  slug: string
-  genreid: string
-  music: string
-  _id: string
-  image: string
-  name: string
-  moodid: string
-  price: number
-  */
   return (
     <div className="bg-black">
       <div className="flex justify-center">
         <div className="w-1/2">
-          <h1 className="text-4xl text-white p-4 font-bold capitalize  text-center">
+          <h1 className="text-4xl text-white p-4 font-bold capitalize text-center">
             {music.name}
           </h1>
           <div className="flex justify-center">
