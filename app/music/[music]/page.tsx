ac0aa210e1e2a205a8554fd3c38411d00acac31c
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-02-19 15:49:58 +0530

    Refactor music and genre display on page.tsx

--- app/music/[music]/page.tsx
+++ app/music/[music]/page.tsx
@@ -13,42 +13,76 @@ import Link from "next/link";
 const TournamentPage = async ({ params }: { params: { music: string } }) => {
   const mood = await getMoodBySlug(params.music);
   const genre = await getGenreBySlug(params.music);
-  const artist = await getAuth();
   // const gameimage = mood.image;
   const music = await getMusic();
   return (
     <main className="bg-black">
-      <div className=" mb-32">
-        <Image
-          src={mood.image}
-          alt="Mood Images"
-          height={400}
-          width={400}
-          className="w-full h-[500px]"
-        />
-        <div className="text-center pt-4">
-          <h1 className="text-4xl">Musics</h1>
-          {music?.length > 0 ? (
-            music.map((p) =>
-              p.moodid === mood._id ? (
-                <Link href={`/music/${mood.slug}/${p.slug}`} key={p._id}>
-                  {p.artist.map((a) => (
-                    <GameBlock
-                      key={p._id}
-                      name={p.name}
-                      image={p.image}
-                      _id={a._ref}
-                      price={p.price}
-                    />
-                  ))}
-                </Link>
-              ) : null
-            )
-          ) : (
-            <div>No Music found</div>
-          )}
+      {mood && (
+        <div className=" mb-32">
+          <Image
+            src={mood.image}
+            alt="Mood Images"
+            height={400}
+            width={400}
+            className="w-full h-[500px]"
+          />
+          <div className="text-center pt-4">
+            <h1 className="text-4xl">{mood.name} Music</h1>
+            {music?.length > 0 ? (
+              music.map((p) =>
+                p.genreid === genre._id ? (
+                  <Link href={`/music/${mood.slug}/${p.slug}`} key={p._id}>
+                    {p.artist.map((a) => (
+                      <GameBlock
+                        key={p._id}
+                        name={p.name}
+                        image={p.image}
+                        _id={a._ref}
+                        price={p.price}
+                      />
+                    ))}
+                  </Link>
+                ) : null
+              )
+            ) : (
+              <div>No Music found</div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
+      {genre && (
+        <div className=" mb-32">
+          <Image
+            src={genre.image}
+            alt="Mood Images"
+            height={400}
+            width={400}
+            className="w-full h-[500px]"
+          />
+          <div className="text-center pt-4">
+            <h1 className="text-4xl">{genre.name} Music</h1>
+            {music?.length > 0 ? (
+              music.map((p) =>
+                p.genreid === genre._id ? (
+                  <Link href={`/music/${mood.slug}/${p.slug}`} key={p._id}>
+                    {p.artist.map((a) => (
+                      <GameBlock
+                        key={p._id}
+                        name={p.name}
+                        image={p.image}
+                        _id={a._ref}
+                        price={p.price}
+                      />
+                    ))}
+                  </Link>
+                ) : null
+              )
+            ) : (
+              <div>No Music found</div>
+            )}
+          </div>
+        </div>
+      )}
     </main>
   );
 };
